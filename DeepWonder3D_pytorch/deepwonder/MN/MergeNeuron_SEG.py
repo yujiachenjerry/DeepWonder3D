d1390a5227053b70a26220c10bbdf9dbdc3e3e8a
Yujia Chen <yujiachenjerry@gmail.com>
2025-08-27 00:35:16 +0800

    Formatting codes

--- DeepWonder3D_pytorch/deepwonder/MN/MergeNeuron_SEG.py
+++ DeepWonder3D_pytorch/deepwonder/MN/MergeNeuron_SEG.py
@@ -12,28 +12,29 @@ import os
 from .merge_neuron_f import Split_Neuron, listAddtrace
 from .merge_neuron_f import listAddcontours_Laplacian_pytorch, list2contours
 from .merge_neuron_f import group_mask, calculate_trace, Mining_rest_neuron, clear_neuron
-from .merge_neuron_f import Neuron_List_Initial, list2mask, listAddtrace, centroid_distance, list_union, initial_mask_list
+from .merge_neuron_f import Neuron_List_Initial, list2mask, listAddtrace, centroid_distance, list_union, \
+    initial_mask_list
 from .merge_neuron_f import list2contours, Joint_Mask_List_Simple, Joint_Mask_List_Mul
 import multiprocessing as mp
 
 
 def otsu(gray):
     pixel_number = gray.shape[0] * gray.shape[1]
-    mean_weigth = 1.0/pixel_number
+    mean_weigth = 1.0 / pixel_number
     # 发现bins必须写到257，否则255这个值只能分到[254,255)区间
-    his, bins = np.histogram(gray, np.arange(0,257))
+    his, bins = np.histogram(gray, np.arange(0, 257))
     final_thresh = -1
     final_value = -1
     intensity_arr = np.arange(256)
-    for t in bins[1:-1]: # This goes from 1 to 254 uint8 range (Pretty sure wont be those values)
+    for t in bins[1:-1]:  # This goes from 1 to 254 uint8 range (Pretty sure wont be those values)
         pcb = np.sum(his[:t])
         pcf = np.sum(his[t:])
         Wb = pcb * mean_weigth
         Wf = pcf * mean_weigth
 
-        mub = np.sum(intensity_arr[:t]*his[:t]) / float(pcb)
-        muf = np.sum(intensity_arr[t:]*his[t:]) / float(pcf)
-        #print mub, muf
+        mub = np.sum(intensity_arr[:t] * his[:t]) / float(pcb)
+        muf = np.sum(intensity_arr[t:] * his[t:]) / float(pcf)
+        # print mub, muf
         value = Wb * Wf * (mub - muf) ** 2
 
         if value > final_value:
@@ -46,19 +47,18 @@ def otsu(gray):
     return final_img, final_thresh
 
 
-
-def merge_neuron_SEG_mul_inten(mask_stack, 
-                    raw_image, 
-                    quit_round_rate = 0.8, 
-                    good_round_rate = 0.9,
-                    good_round_size_rate = 0.8, 
-                    corr_mark=0.9, 
-                    max_value=1000,
-                    seg_thres = 0.7,
-                    if_nmf = True,
-                    inten_thres = 0.01,
-                    edge_value = 20,
-                    smallest_neuron_area = 36):
+def merge_neuron_SEG_mul_inten(mask_stack,
+                               raw_image,
+                               quit_round_rate=0.8,
+                               good_round_rate=0.9,
+                               good_round_size_rate=0.8,
+                               corr_mark=0.9,
+                               max_value=1000,
+                               seg_thres=0.7,
+                               if_nmf=True,
+                               inten_thres=0.01,
+                               edge_value=20,
+                               smallest_neuron_area=36):
     # 1um 144
     # 2um 36
 
@@ -121,14 +121,16 @@ def merge_neuron_SEG_mul_inten(mask_stack,
     time_cost = time.time() - time_start
     # print('[Neuron Segmentation Mask Initialize Time Cost: %.0d s] \n'%(time_cost), end=' ')
     # 是否加速
-    if_mul=1
+    if_mul = 1
     if if_mul:
         num_cores = int(mp.cpu_count())
         pool = mp.Pool(num_cores)
         mask_dict = {}
         for i in range(0, mask_stack.shape[0]):
-            mask_dict[str(i)] = mask_stack[i,:,:].squeeze()
-        results = [pool.apply_async(initial_mask_list, args=(mask, quit_round_rate, good_round_rate, smallest_neuron_area)) for name, mask in mask_dict.items()]
+            mask_dict[str(i)] = mask_stack[i, :, :].squeeze()
+        results = [
+            pool.apply_async(initial_mask_list, args=(mask, quit_round_rate, good_round_rate, smallest_neuron_area)) for
+            name, mask in mask_dict.items()]
         for p in results:
             good_neuron_list, bad_neuron_list = p.get()
             w_good_neuron_list = good_neuron_list + w_good_neuron_list
@@ -136,7 +138,7 @@ def merge_neuron_SEG_mul_inten(mask_stack,
             # print('good_neuron_list ---> ',len(good_neuron_list),' bad_neuron_list ---> ',len(bad_neuron_list))
         # print('w_good_neuron_list ---> ',len(w_good_neuron_list),' w_bad_neuron_list ---> ',len(w_bad_neuron_list))
         time_cost = time.time() - time_start
-        print('[Neuron Segmentation Mask Initialize Time Cost: %.0d s] \n'%(time_cost), end=' ')
+        print('[Neuron Segmentation Mask Initialize Time Cost: %.0d s] \n' % time_cost, end=' ')
         pool.close()
         pool.join()
 
@@ -148,33 +150,33 @@ def merge_neuron_SEG_mul_inten(mask_stack,
     if_time = True
     if if_time:
         time_cost = time.time() - time_start
-        print('[1 Time Cost: %.0d s] \n'%(time_cost), end=' ')
-    
+        print('[1 Time Cost: %.0d s] \n' % time_cost, end=' ')
+
     if_split = True
-    w_good_neuron_list = Joint_Mask_List_Mul(w_good_neuron_list, 
-                                            corr_mark=0.9, 
-                                            area_mark=0.9, 
-                                            active_rate=0, 
-                                            if_coor=True, 
-                                            if_area=True, 
-                                            if_merge=False)
+    w_good_neuron_list = Joint_Mask_List_Mul(w_good_neuron_list,
+                                             corr_mark=0.9,
+                                             area_mark=0.9,
+                                             active_rate=0,
+                                             if_coor=True,
+                                             if_area=True,
+                                             if_merge=False)
     w_n_list_init = w_good_neuron_list
     w_g_neuron_list = w_good_neuron_list
 
     time_cost = time.time() - time_start
-    print('[2 Time Cost: %.0d s] \n'%(time_cost), end=' ')
+    print('[2 Time Cost: %.0d s] \n' % (time_cost), end=' ')
     # print('w_g_neuron_list ---> ',len(w_g_neuron_list))
-    w_g_neuron_list = Joint_Mask_List_Simple(w_g_neuron_list, 
-                                            [], 
-                                            corr_mark=0.7, 
-                                            area_mark=0.6, 
-                                            active_rate=0, 
-                                            if_coor=False, 
-                                            if_area=True, 
-                                            if_merge=False)
-        # print(w_g_neuron_list)
+    w_g_neuron_list = Joint_Mask_List_Simple(w_g_neuron_list,
+                                             [],
+                                             corr_mark=0.7,
+                                             area_mark=0.6,
+                                             active_rate=0,
+                                             if_coor=False,
+                                             if_area=True,
+                                             if_merge=False)
+    # print(w_g_neuron_list)
     time_cost = time.time() - time_start
-    print('[3 Time Cost: %.0d s] \n'%(time_cost), end=' ')
+    print('[3 Time Cost: %.0d s] \n' % time_cost, end=' ')
     # print('w_g_neuron_list ---> ',len(w_g_neuron_list))
     # print('mask -----> ','mask' in w_g_neuron_list[0])
     from .merge_neuron_f import neuron_max_filter, list_add_mask, delete_edge_neuron
@@ -182,41 +184,37 @@ def merge_neuron_SEG_mul_inten(mask_stack,
 
     all_neuron_mask = list_add_mask(w_g_neuron_list, raw_image)
     time_cost = time.time() - time_start
-    print('[4 Time Cost: %.0d s] \n'%(time_cost), end=' ')
+    print('[4 Time Cost: %.0d s] \n' % time_cost, end=' ')
     # remain_mask part is so slow
     # all_neuron_remain_mask = remain_mask_test_list(w_g_neuron_list)
     # all_neuron_remain_mask = remain_mask(all_neuron_mask) 
     all_neuron_remain_mask = all_neuron_mask
     time_cost = time.time() - time_start
-    print('[5 Time Cost: %.0d s] \n'%(time_cost), end=' ')
+    print('[5 Time Cost: %.0d s] \n' % time_cost, end=' ')
     # w_g_neuron_list1 = w_g_neuron_list #.clone()
     if 0:
         w_g_neuron_list = neuron_max_filter(w_g_neuron_list,
                                             all_neuron_mask,
                                             raw_image_max,
-                                            max_thres = inten_thres)
+                                            max_thres=inten_thres)
     time_cost = time.time() - time_start
-    print('[6 Time Cost: %.0d s] \n'%(time_cost), end=' ')
+    print('[6 Time Cost: %.0d s] \n' % time_cost, end=' ')
     # print('w_g_neuron_list ---> ',len(w_g_neuron_list))
     # print('mask -----> ','mask' in w_g_neuron_list[0]) 
     w_g_neuron_list = neuron_max_filter(w_g_neuron_list,
                                         all_neuron_mask,
                                         raw_image_std,
-                                        max_thres = inten_thres)
+                                        max_thres=inten_thres)
     w_g_neuron_list = delete_edge_neuron(w_g_neuron_list,
-                                        all_neuron_mask,
-                                        edge_value=edge_value)
+                                         all_neuron_mask,
+                                         edge_value=edge_value)
     time_cost = time.time() - time_start
-    print('[7 Time Cost: %.0d s] \n'%(time_cost), end=' ')
+    print('[7 Time Cost: %.0d s] \n' % time_cost, end=' ')
     # print('w_g_neuron_list ---> ',len(w_g_neuron_list))
     # print('mask -----> ','mask' in w_g_neuron_list[0])
     w_g_neuron_list = add_remain_mask_list(w_g_neuron_list, all_neuron_remain_mask)
     # print('remain_position ---> ',w_g_neuron_list[0]['remain_position'][0,:])
     # print('mask -----> ','mask' in w_g_neuron_list[0])
     time_cost = time.time() - time_start
-    print('[8 Time Cost: %.0d s] \n'%(time_cost), end=' ')
-    return w_g_neuron_list, w_n_list_init, all_neuron_mask, all_neuron_remain_mask, mask_stack_filted, mask_stack_max
-
-
-
-
+    print('[8 Time Cost: %.0d s] \n' % time_cost, end=' ')
+    return w_g_neuron_list, w_n_list_init, all_neuron_mask, all_neuron_remain_mask, mask_stack_filted
--- DeepWonder3D_pytorch/deepwonder/test_MN.py
+++ DeepWonder3D_pytorch/deepwonder/test_MN.py
@@ -18,8 +18,11 @@ from deepwonder.MN.merge_neuron_f import listAddtrace, listAdd_remain_trace
 import scipy.io as scio
 
 import warnings
-warnings.filterwarnings('ignore') 
+
+warnings.filterwarnings('ignore')
 from deepwonder.utils import save_img_train, save_para_dict, UseStyle, save_img, get_netpara
+
+
 #############################################################################################################################################
 #############################################################################
 #############################################################################
@@ -56,29 +59,27 @@ class calculate_neuron():
     #########################################################################
     #########################################################################
     def make_folder(self):
-        current_time = 'MN_'+self.RMBG_datasets_folder+'_'+self.SEG_datasets_folder
-        self.MN_output_path = self.MN_output_dir + '//' + self.MN_output_folder #current_time.replace('//','_')
+        current_time = 'MN_' + self.RMBG_datasets_folder + '_' + self.SEG_datasets_folder
+        self.MN_output_path = self.MN_output_dir + '//' + self.MN_output_folder  # current_time.replace('//','_')
         # print('self.MN_output_path ----> ',self.MN_output_path)
-        if not os.path.exists(self.MN_output_dir): 
+        if not os.path.exists(self.MN_output_dir):
             os.mkdir(self.MN_output_dir)
-        if not os.path.exists(self.MN_output_path): 
+        if not os.path.exists(self.MN_output_path):
             os.mkdir(self.MN_output_path)
-
 
     #########################################################################
     #########################################################################
     def save_para(self):
         yaml_dict = self.__dict__.copy()
         # del yaml_dict['optimizer'] 
-        yaml_name = 'MN_para'+'.yaml'
-        save_MN_para_path = self.MN_output_path+ '//'+yaml_name
+        yaml_name = 'MN_para' + '.yaml'
+        save_MN_para_path = self.MN_output_path + '//' + yaml_name
         save_para_dict(save_MN_para_path, yaml_dict)
-
 
     #########################################################################
     #########################################################################
     def generate_patch(self):
-        RMBG_folder = self.RMBG_datasets_path+'//'+self.RMBG_datasets_folder
+        RMBG_folder = self.RMBG_datasets_path + '//' + self.RMBG_datasets_folder
         # print('\n Merge Neuron Part >>> RMBG_folder : ',RMBG_folder)
         # print(list(os.walk(RMBG_folder, topdown=False))[-1][-1])
         RMBG_name_list = []
@@ -87,12 +88,12 @@ class calculate_neuron():
             # print('im_name -----> ',im_name)
             if '.tif' in RMBG_name:
                 RMBG_name_list.append(RMBG_name)
-                RMBG_dir = RMBG_folder+'//'+RMBG_name
+                RMBG_dir = RMBG_folder + '//' + RMBG_name
                 # RMBG_im = tiff.imread(RMBG_dir)
                 # RMBG_list[RMBG_name] = RMBG_im
                 RMBG_list[RMBG_name] = RMBG_dir
 
-        SEG_folder = self.SEG_datasets_path+'//'+self.SEG_datasets_folder
+        SEG_folder = self.SEG_datasets_path + '//' + self.SEG_datasets_folder
         # print('\n Merge Neuron Part >>> SEG_folder : ',SEG_folder)
         # print('SEG_folder : ',SEG_folder)
         SEG_name_list = []
@@ -101,17 +102,17 @@ class calculate_neuron():
             # print('im_name -----> ',im_name)
             if '.tif' in SEG_name:
                 SEG_name_list.append(SEG_name)
-                SEG_dir = SEG_folder+'//'+SEG_name
+                SEG_dir = SEG_folder + '//' + SEG_name
                 # SEG_im = tiff.imread(SEG_dir)
                 SEG_list[SEG_name] = SEG_dir
                 # print(RMBG_name,' ---> ',SEG_dir)
-        
+
         self.RMBG_name_list = RMBG_name_list
         self.RMBG_list = RMBG_list
         self.SEG_name_list = SEG_name_list
         self.SEG_list = SEG_list
 
-        SR_folder = self.SR_datasets_path+'//'+self.SR_datasets_folder
+        SR_folder = self.SR_datasets_path + '//' + self.SR_datasets_folder
         # print('SR_folder : ',SR_folder)
         SR_name_list = []
         SR_list = {}
@@ -120,7 +121,7 @@ class calculate_neuron():
             # print('im_name -----> ',im_name)
             if '.tif' in SR_name:
                 SR_name_list.append(SR_name)
-                SR_dir = SR_folder+'//'+SR_name
+                SR_dir = SR_folder + '//' + SR_name
                 # SEG_im = tiff.imread(SEG_dir)
                 SR_list[SR_name] = SR_dir
                 # print(RMBG_name,' ---> ',SEG_dir)
@@ -131,10 +132,9 @@ class calculate_neuron():
         # print('SEG : ',self.SEG_list)
         # realign_mov_w_bg_LFM_v_162.0.tif
         # print('SEG : ',SEG_list[self.SEG_name_list[0]])
-        return self.RMBG_name_list, self.RMBG_list, 
+        return self.RMBG_name_list, self.RMBG_list,
         self.SEG_name_list, self.SEG_list,
         self.SR_name_list, self.SR_list
-
 
     #########################################################################
     #########################################################################
@@ -146,7 +146,7 @@ class calculate_neuron():
         # print('SEG dir : ',SEG_dir)
         # print('SR dir : ',SR_dir)
         img_SEG = tiff.imread(SEG_dir)
-        print('img_SEG ---> ',img_SEG.shape)
+        print('img_SEG ---> ', img_SEG.shape)
         # img_SEG = img_SEG[0:2, :, :]
         img_RMBG = tiff.imread(RMBG_dir)
         img_SR = tiff.imread(SR_dir)
@@ -158,16 +158,16 @@ class calculate_neuron():
         # whole_mask_list = merge_neuron_SEG_mul(img_SEG1, img_RMBG1)
         self.inten_thres = 0
         whole_mask_list, test_list, all_neuron_mask, all_neuron_remain_mask, \
-        mask_stack_filted, mask_stack_max = merge_neuron_SEG_mul_inten(img_SEG1, 
-                                        img_RMBG1, 
-                                        quit_round_rate = 0.5, 
-                                        good_round_rate = 0.8,
-                                        good_round_size_rate = 0.5, 
-                                        corr_mark=0.9, 
-                                        max_value=1000 ,
-                                        if_nmf = False,
-                                        inten_thres = self.inten_thres,
-                                        edge_value = 10)
+            mask_stack_filted = merge_neuron_SEG_mul_inten(img_SEG1,
+                                                           img_RMBG1,
+                                                           quit_round_rate=0.5,
+                                                           good_round_rate=0.8,
+                                                           good_round_size_rate=0.5,
+                                                           corr_mark=0.9,
+                                                           max_value=1000,
+                                                           if_nmf=False,
+                                                           inten_thres=self.inten_thres,
+                                                           edge_value=10)
         # print('whole_mask_list -----> ',len(whole_mask_list))
         # print('remain_position ---> ',whole_mask_list[0]['remain_position'][0,:])
         # print('mask -----> ','mask' in whole_mask_list[0])
@@ -185,54 +185,53 @@ class calculate_neuron():
         whole_mask_list = listAdd_remain_trace(whole_mask_list, img_RMBG, mode='update', trace_mode='all')
         # print('mask -----> ','mask' in whole_mask_list[0])
         # print(whole_mask_list[0].keys())
-        whole_mask_list = listAdd_remain_trace(whole_mask_list, img_SR, dict_name='remain_sr_trace', mode='update', trace_mode='all')
+        whole_mask_list = listAdd_remain_trace(whole_mask_list, img_SR, dict_name='remain_sr_trace', mode='update',
+                                               trace_mode='all')
         # print(whole_mask_list[0].keys())
 
-        
         # print('mask -----> ','mask' in whole_mask_list[0])
-        final_contours,whole_contours = list2contours(whole_mask_list, img_RMBG.shape[1], img_RMBG.shape[2])
+        final_contours, whole_contours = list2contours(whole_mask_list, img_RMBG.shape[1], img_RMBG.shape[2])
 
-        f_con_output_path = self.MN_output_path+'//f_con'+'_'+str(self.inten_thres)
-        if not os.path.exists(f_con_output_path): 
+        f_con_output_path = self.MN_output_path + '//f_con' + '_' + str(self.inten_thres)
+        if not os.path.exists(f_con_output_path):
             os.mkdir(f_con_output_path)
-        img_f_contours_name = f_con_output_path+'//'+im_name+'_f_con.tif'
+        img_f_contours_name = f_con_output_path + '//' + im_name + '_f_con.tif'
         final_contours = final_contours.clip(0, 65535).astype('uint16')
         io.imsave(img_f_contours_name, final_contours)
 
         from deepwonder.MN.merge_neuron_f import list2masks
-        f_mask_bina_output_path = self.MN_output_path+'//f_mask_bina'+'_'+str(self.inten_thres)
-        if not os.path.exists(f_mask_bina_output_path): 
+        f_mask_bina_output_path = self.MN_output_path + '//f_mask_bina' + '_' + str(self.inten_thres)
+        if not os.path.exists(f_mask_bina_output_path):
             os.mkdir(f_mask_bina_output_path)
 
         # print('mask -----> ','mask' in whole_mask_list[0])
         final_masks, whole_masks = list2masks(whole_mask_list, img_RMBG.shape[1], img_RMBG.shape[2])
         final_masks_bina = final_masks
-        final_masks_bina[final_masks_bina>0]=1
-        img_f_masks_bina_name = f_mask_bina_output_path+'//'+im_name+'_f_mask_bina.tif'
+        final_masks_bina[final_masks_bina > 0] = 1
+        img_f_masks_bina_name = f_mask_bina_output_path + '//' + im_name + '_f_mask_bina.tif'
         final_masks_bina = final_masks_bina.clip(0, 65535).astype('uint16')
         io.imsave(img_f_masks_bina_name, final_masks_bina)
 
-        w_con_output_path = self.MN_output_path+'//w_con'+'_'+str(self.inten_thres)
-        if not os.path.exists(w_con_output_path): 
+        w_con_output_path = self.MN_output_path + '//w_con' + '_' + str(self.inten_thres)
+        if not os.path.exists(w_con_output_path):
             os.mkdir(w_con_output_path)
-        img_w_contours_name = w_con_output_path+'//'+im_name+'_w_con.tif'
+        img_w_contours_name = w_con_output_path + '//' + im_name + '_w_con.tif'
         whole_contours = whole_contours.clip(0, 65535).astype('uint16')
 
         if 0:
-            if len(whole_contours.shape)==3:
-                if len(whole_mask_list)>0:
+            if len(whole_contours.shape) == 3:
+                if len(whole_mask_list) > 0:
                     io.imsave(img_w_contours_name, whole_contours)
 
-
         if 0:
-            img_f_contours_name = self.MN_output_path+'//'+im_name+'_mask_stack.tif'
+            img_f_contours_name = self.MN_output_path + '//' + im_name + '_mask_stack.tif'
             all_neuron_mask = all_neuron_mask.clip(0, 65535).astype('uint16')
             io.imsave(img_f_contours_name, all_neuron_mask)
 
             # test_list = listAddtrace(test_list, img_RMBG, mode='update', trace_mode='all')
             # test_list = listAddcontours_Laplacian_pytorch(test_list, img_RMBG.shape[1], img_RMBG.shape[2])
             # test_contours,test_whole_contours = list2contours(test_list, img_RMBG.shape[1], img_RMBG.shape[2])
-            test_contours_name = self.MN_output_path+'//'+im_name+'_test_con.tif'
+            test_contours_name = self.MN_output_path + '//' + im_name + '_test_con.tif'
             all_neuron_remain_mask = all_neuron_remain_mask.clip(0, 65535).astype('uint16')
             io.imsave(test_contours_name, all_neuron_remain_mask)
 
@@ -247,28 +246,27 @@ class calculate_neuron():
             io.imsave(img_f_masks_bina_name, test_list_masks)
             '''
 
-        mat_output_path = self.MN_output_path+'//mat'+'_'+str(self.inten_thres)
-        if not os.path.exists(mat_output_path): 
+        mat_output_path = self.MN_output_path + '//mat' + '_' + str(self.inten_thres)
+        if not os.path.exists(mat_output_path):
             os.mkdir(mat_output_path)
-        mat_save_name = mat_output_path+'//'+im_name+'.mat'
+        mat_save_name = mat_output_path + '//' + im_name + '.mat'
         # data = {'a':whole_mask_list, 'final_contours':final_contours}
         # print('mask -----> ','mask' in whole_mask_list[0])
         # print(whole_mask_list[0]['mask'])
-        for i in range(0,len(whole_mask_list)):
+        for i in range(0, len(whole_mask_list)):
             single_neuron = whole_mask_list[i]
             del single_neuron['mask']
         # print('mask -----> ','mask' in whole_mask_list[0])
         # print(whole_mask_list[0].keys())
-        scio.savemat(mat_save_name, {'final_mask_list':whole_mask_list, 'final_contours':final_contours})
+        scio.savemat(mat_save_name, {'final_mask_list': whole_mask_list, 'final_contours': final_contours})
 
-        img_w_contours_name = self.MN_output_path+'//'+im_name+'.tif'
+        img_w_contours_name = self.MN_output_path + '//' + im_name + '.tif'
         whole_contours = whole_contours.clip(0, 65535).astype('uint16')
-
 
     #########################################################################
     #########################################################################
     def run(self):
-        for im_index in range(0,len(self.RMBG_name_list)): 
+        for im_index in range(0, len(self.RMBG_name_list)):
             im_name = self.RMBG_name_list[im_index]
             # print('im_name : ', im_name)
             img_RMBG = self.RMBG_list[im_name]
@@ -276,20 +274,21 @@ class calculate_neuron():
             img_SR = self.SR_list[im_name]
             self.get_neuron_mask(img_SEG, img_RMBG, img_SR, im_name)
 
-if __name__ == '__main__':
-    MN_parameters_test = { 'RMBG_datasets_folder' : 'RMBG',
-                            'RMBG_datasets_path' : 'test_results//SR_test_07192023_2_113_up12',
-                            ###########################
-                            'SEG_datasets_folder' : 'SEG',
-                            'SEG_datasets_path' : 'test_results//SR_test_07192023_2_113_up12',
-                            ###########################
-                            'SR_datasets_folder' : 'pred_signal',
-                            'SR_datasets_path' : 'test_results//SR_test_07192023_2_113_up12',
-                            'MN_output_dir' : 'test_results',
-                        }
 
-    MN_parameters_test['MN_output_folder'] = 'MN_'+MN_parameters_test['RMBG_datasets_folder']+\
-    '_'+MN_parameters_test['SEG_datasets_folder']
+if __name__ == '__main__':
+    MN_parameters_test = {'RMBG_datasets_folder': 'RMBG',
+                          'RMBG_datasets_path': 'test_results//SR_test_07192023_2_113_up12',
+                          ###########################
+                          'SEG_datasets_folder': 'SEG',
+                          'SEG_datasets_path': 'test_results//SR_test_07192023_2_113_up12',
+                          ###########################
+                          'SR_datasets_folder': 'pred_signal',
+                          'SR_datasets_path': 'test_results//SR_test_07192023_2_113_up12',
+                          'MN_output_dir': 'test_results',
+                          }
+
+    MN_parameters_test['MN_output_folder'] = 'MN_' + MN_parameters_test['RMBG_datasets_folder'] + \
+                                             '_' + MN_parameters_test['SEG_datasets_folder']
     # self.MN_output_path = self.MN_output_dir + '//' + self.MN_output_folder
     MN_model = calculate_neuron(MN_parameters_test)
     MN_model.run()
